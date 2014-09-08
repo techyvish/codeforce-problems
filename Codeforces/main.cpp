161c8fd8bfdf85e6c6d7761bf27a758afdfe782f
Vishal <vishal.x.patel@nab.com.au>
2014-09-09 08:19:00 +1000

    Initial Commit

---
+++ Codeforces/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Codeforces
+//
+//  Created by Vishal Patel on 9/09/2014.
+//  Copyright (c) 2014 Vishal Patel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
