0ede13a0e6b48ad64cc159e89226323f84ae4d85
Vishal <vishal.x.patel@nab.com.au>
2014-10-11 10:33:01 +1100

    [UVA-OnlineJudge].

---
+++ Codeforces/uva1124.cpp
@@ -0,0 +1,93 @@
+//
+//  uva1124.cpp
+//  Codeforces
+//
+//  Created by Vishal Patel on 10/10/2014.
+//  Copyright (c) 2014 Vishal Patel. All rights reserved.
+//
+
+//
+//  ScanInput.cpp
+//  Codeforces
+//
+//  Created by Vishal Patel on 10/10/2014.
+//  Copyright (c) 2014 Vishal Patel. All rights reserved.
+//
+
+#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<string> vs;
+
+// Basic macros
+#define st          first
+#define se          second
+#define all(x)      (x).begin(), (x).end()
+#define ini(a, v)   memset(a, v, sizeof(a))
+#define re(i,s,n)  	for(int i=s;i<(n);++i)
+#define rep(i,s,n)  for(int i=s;i<=(n);++i)
+#define fr(i,n)     re(i,0,n)
+#define repv(i,f,t) for(int i = f; i >= t; --i)
+#define rev(i,f,t)  repv(i,f - 1,t)
+#define frv(i,n)    rev(i,n,0)
+#define pu          push_back
+#define mp          make_pair
+#define sz(x)       (int)(x.size())
+
+const int oo = 2000000009;
+const double eps = 1e-9;
+
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+
+#else
+
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+
+#endif
+
+//int main()
+//{
+//   
+//    string str;
+//    vector<string> stack;
+//    while ( getline(cin, str)) {
+//        stack.push_back(str);
+//    }
+//    
+//    for ( int i = 0 ; i < stack.size() ; i++)
+//    {
+//        cout << stack[i] << endl;
+//    }
+//    return 0;
+//}
