e1c569093b24d3f09ca13b2c42e4722a54a9dbc7
Vishal <vishal.x.patel@nab.com.au>
2014-10-15 08:24:52 +1100

    uva 230

--- Codeforces/scanregex.cpp
+++ Codeforces/scanregex.cpp
@@ -61,7 +61,7 @@ typedef vector<string> vs;
 #define FORi(i,a,b) for(LET(i,a) ; i<b; i++)
 #define repi(i,n) FORi(i,(__typeof(n))0,n)
 #define FOR(i,a,b) for(i=a ; i<b; i++)
-#define rep(i,n) FOR(i,0,n)
+//#define rep(i,n) FOR(i,0,n)
 #define si(n) scanf("%d",&n)
 #define sll(n) scanf("%lld",&n)
 #define pi(n) printf("%d",n)
@@ -93,46 +93,47 @@ const double eps = 1e-9;
 #define trace6(a, b, c, d, e, f)
 
 #endif
-//#define fin cin
-int main()
-{
-    //FILE *fp = freopen("/Users/Shared/codeforces/codeforces/in.txt", "rt", stdin);
-    fstream fin("/Users/Shared/codeforces/codeforces/scantest.txt");
-    
-    char sti[1024];
-    string buffstr;
-    while (getline(fin, buffstr) && sscanf(buffstr.c_str(), "%[^\n\r]",sti) == 1) {
-        if ( buffstr == "END")
-            break;
-        cout << sti << endl;
-    }
-    
-    while (getline(fin, buffstr) && sscanf(buffstr.c_str(), "%[A-Z]",sti) == 1) {
-        if ( buffstr == "END")
-            break;
-        cout << sti << endl;
-    }
-    
-    
-    //%*[^(]   read and discard everything up to opening paren
-    //(        read and discard the opening paren
-    //%[^)]    read and store up up to (but not including) the closing paren
-    //%*[^\n]  read and discard up to (but not including) the newline
-    while (getline(fin, buffstr) && sscanf(buffstr.c_str(), "%*[^(](%[^)]%*[^\n\r]",sti) == 1) {
-        if ( buffstr == "END")
-            break;
-        cout << sti << endl;
-    }
-    
-    while (getline(fin, buffstr) && sscanf(buffstr.c_str(), "TEST FOR \"%[^\"]%*[^\n\r]",sti) == 1) {
-        if ( buffstr == "END")
-            break;
-        cout << sti << endl;
-    }
-    
-    while (getline(fin, buffstr) && sscanf(buffstr.c_str(), "TEST FOR \"%[^\"]%*[^\n\r]",sti) == 1) {
-        if ( buffstr == "END")
-            break;
-        cout << sti << endl;
-    }
-}
+#define fin cin
+
+//int main()
+//{
+//    //FILE *fp = freopen("/Users/Shared/codeforces/codeforces/in.txt", "rt", stdin);
+//    //fstream fin("/Users/Shared/codeforces/codeforces/scantest.txt");
+//    
+//    char sti[1024];
+//    string buffstr;
+//    while (getline(fin, buffstr) && sscanf(buffstr.c_str(), "%[^\n\r]",sti) == 1) {
+//        if ( buffstr == "END")
+//            break;
+//        cout << sti << endl;
+//    }
+//    
+//    while (getline(fin, buffstr) && sscanf(buffstr.c_str(), "%[A-Z]",sti) == 1) {
+//        if ( buffstr == "END")
+//            break;
+//        cout << sti << endl;
+//    }
+//    
+//    //%*[^(]   read and discard everything up to opening paren
+//    //(        read and discard the opening paren
+//    //%[^)]    read and store up up to (but not including) the closing paren
+//    //%*[^\n]  read and discard up to (but not including) the newline
+//    
+//    while (getline(fin, buffstr) && sscanf(buffstr.c_str(), "%*[^(](%[^)]%*[^\n\r]",sti) == 1) {
+//        if ( buffstr == "END")
+//            break;
+//        cout << sti << endl;
+//    }
+//    
+//    while (getline(fin, buffstr) && sscanf(buffstr.c_str(), "TEST FOR \"%[^\"]%*[^\n\r]",sti) == 1) {
+//        if ( buffstr == "END")
+//            break;
+//        cout << sti << endl;
+//    }
+//    
+//    while (getline(fin, buffstr) && sscanf(buffstr.c_str(), "TEST FOR \"%[^\"]%*[^\n\r]",sti) == 1) {
+//        if ( buffstr == "END")
+//            break;
+//        cout << sti << endl;
+//    }
+//}
