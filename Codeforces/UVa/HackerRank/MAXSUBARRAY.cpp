8482329265e6ddbf635dcff37d7368b4f45e0606
Vishal Patel <vis8051@gmail.com>
2016-07-13 16:24:53 +1000

    * Problems on max subarray sum.

--- Codeforces/UVa/HackerRank/MAXSUBARRAY.cpp
+++ Codeforces/UVa/HackerRank/MAXSUBARRAY.cpp
@@ -13,7 +13,7 @@ typedef struct {
     int i,j,max;
 } maxx;
 
-int main() {
+int main_maxsumsubarray() {
 
     int n, t, tc, a[1000000];
     fstream fin("../HackerRank/MAXSUBARRAY.txt");
@@ -26,15 +26,21 @@ int main() {
             fin >> a[i];
         }
 
-        maxx result;
+        maxx result = {0,0,0};
+        int non_cont_sum = 0 ;
+        int max_negative = -INT_MAX;
         int max_so_far = 0, max_ending_here = 0;
         for ( int i = 0; i < n ; i ++ ) {
+            if ( a[i] > 0 ) {
+                non_cont_sum += a[i];
+            } else if ( a[i] > max_negative ){
+                max_negative = a[i];
+            }
             max_ending_here = max_ending_here + a[i];
             if ( max_ending_here < 0 ) {
                 max_ending_here = 0 ;
                 result.i = i;
                 result.j = i;
-                result.max = max_ending_here;
             } else if ( max_so_far < max_ending_here ) {
                 max_so_far = max_ending_here;
                 result.j = i;
@@ -42,7 +48,20 @@ int main() {
             }
         }
 
-        cout << "i: " << result.i << " j: " << result.j << " max: " << result.max << endl;
+        int max2 = 0 ;
+        if (non_cont_sum > 0 ){
+            max2 = non_cont_sum;
+        }else {
+            max2 = max_negative;
+        }
+        int max1 = 0 ;
+        if ( result.max > 0 ){
+            max1 = result.max;
+        }else {
+            max1 = max2;
+        }
+        cout << max1 << " " << max2 << endl;
+        //cout << "i: " << result.i << " j: " << result.j << " max: " << result.max << endl;
     }
 
 }
---
+++ Codeforces/UVa/UVA10684.cpp
@@ -0,0 +1,61 @@
+//
+// Created by Vishal Patel on 7/13/16.
+//
+
+
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+#define fin cin
+
+typedef struct _max{
+    int i ;
+    int j ;
+    int max ;
+
+
+}maxx;
+
+int main() {
+
+    int n, t, tc, a[1000000];
+    fstream fin("../UVA10684.txt");
+
+    fin >> n;
+    while ( n != 0 ) {
+        for ( int i = 0 ; i < n ; i ++ ){
+            fin >> a[i];
+        }
+
+        maxx maxres = {0,0,0};
+
+        int max_so_far = 0, max_end_here = 0 ;
+        for ( int i = 0 ; i < n ; i ++ ){
+            max_end_here = max_end_here + a[i];
+            if ( max_end_here < 0 ) {
+                max_end_here = 0;
+                maxres.i = i;
+                maxres.j = i;
+            } else if ( max_so_far < max_end_here ){
+                max_so_far = max_end_here;
+                maxres.j = i;
+                maxres.max = max_so_far;
+            }
+        }
+
+        if ( maxres.max > 0 ) {
+            printf("The maximum winning streak is %d.\n",maxres.max);
+        }else {
+            printf("Losing streak.\n");
+        }
+
+        fin >> n;
+
+    }
+    printf("\n");
+
+}
